3f6ec67343706586dda78e084ae9b8c861b1c2c9
irina08 <irina_ger@yahoo.com>
2020-02-09 00:12:54 -0800

    added README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java-Selenium Project 
+
+### Selenium version: 3.141.59
+### Java version: 1.8
--- src/basicweb/TestClass.java
+++ src/basicweb/TestClass.java
@@ -1,13 +1,15 @@
 package basicweb;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestClass {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
-
+        WebElement element = driver.findElement(By.id(" "));
         //driver.get()
     }
 }
