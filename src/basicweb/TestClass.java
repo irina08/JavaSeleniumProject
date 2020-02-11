d26ea4c9ea8023dc9a3f35f39b8129ad7afd842d
irina08 <irina_ger@yahoo.com>
2020-02-11 01:23:36 -0800

    added some methods

--- src/basicweb/TestClass.java
+++ src/basicweb/TestClass.java
@@ -4,12 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 //import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        String baseURL = "https://learn.letskodeit.com";
         WebDriver driver = new ChromeDriver();
-        WebElement element = driver.findElement(By.id(" "));
-        //driver.get()
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(baseURL);
+
+        driver.findElement(By.xpath("//a[@href='/sign_in']")).click();
+        driver.findElement(By.id("user_email")).sendKeys("letskodeit@gmail.com");
+        driver.findElement(By.name("enter-name")).sendKeys("Automation");
+
+        driver.findElement(By.linkText("Login")).click();
+        Thread.sleep(3000);
+
+        driver.findElement(By.partialLinkText("Pract")).click();
     }
 }
