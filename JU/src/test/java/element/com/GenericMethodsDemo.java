63a6c1402042cd86363d79ee5f3850493c39abf2
irina08 <irina_ger@yahoo.com>
2020-04-30 11:14:49 -0700

    added GenericMethodsDemo.java

---
+++ JU/src/test/java/element/com/GenericMethodsDemo.java
@@ -0,0 +1,42 @@
+package element.com;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class GenericMethodsDemo {
+
+    private WebDriver driver;
+    private String baseUrl;
+    private GenericMethods gm;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        baseUrl = "https://letskodeit.teachable.com/pages/practice";
+        gm = new GenericMethods(driver);
+
+        // Maximize the browser's window
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void testMethod() throws Exception {
+        driver.get(baseUrl);
+
+        WebElement element = gm.getElement("name", "id");
+        element.sendKeys("test");
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
